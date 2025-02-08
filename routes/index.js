a13339a2a4fb400c715de942e8cd77243197c4f2
Keidys Camacho <147742026+keidyli@users.noreply.github.com>
2025-02-08 02:04:33 -0400

    Add files via upload
    subiendo logica de control

---
+++ routes/index.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const router = express.Router();
+const main = require('../models/principal');
+
+/*
+const { getConnection } = require("../data/db");  // Asegúrate de la ruta correcta
+
+// Ruta de prueba para verificar la conexión
+router.get("/test-db", async (req, res) => {
+    try {
+        const connection = await getConnection();
+        const result = await connection.query("SELECT 1 + 1 AS solution");
+        connection.end();  // Cerramos la conexión después de la consulta
+        res.json({ message: "✅ Conexión exitosa", resultado: result });
+    } catch (error) {
+        res.status(500).json({ message: "❌ Error en la conexión", error: error.message });
+    }
+});
+
+*/
+
+// Ruta para mostrar la vista de login
+router.get('/', (req, res) => {
+  res.render('index'); // Renderiza index.ejs desde la carpeta views
+});
+
+// Ruta para procesar el login y manejar la logica
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Validar credenciales desde variables de entorno
+  if (username === process.env.NAME_USER && password === process.env.PASSWORD)
+  {
+    const token= jwt.sign({username}, process.env.JWT_SECRET, { expiresIn: '1h'}); // Credenciales correctas, genera un token
+    res.cookie('jwt', token, {httpOnly: true}); // Guarda el token en una cookie
+    return res.status(200).redirect('/principal');// Redirige a vista principal después del login
+     
+  }
+  res.status(401).send('Credenciales incorrectas');
+});
+
+// Middleware para proteger rutas
+const protectRoute = (req, res, next) => {
+  const token = req.cookies.jwt; //obtiene el token de la cookie
+
+  if (!token) {
+    return res.redirect('/')
+  }
+
+  try {
+    jwt.verify(token, process.env.JWT_SECRET); //verificar la validez del token
+    next();
+  } catch (err) {
+    res.redirect('/'); // si no es valido, devuelve vista login
+  }
+};
+
+// Mostrando Ruta protegida 
+// ✅ Correcto
+router.get('/principal', protectRoute, (req, res) => {
+  main.mostrarProyecto()
+    .then(datos => {
+      res.render('principal', { datos: datos });
+    })
+    .catch(err => {
+      console.error("Error al obtener proyectos:", err); // 👁️ ¡Agrega logs para debuggear!
+      res.render('principal', { datos: {} });
+    });
+});
+
+// Ruta para cerrar sesión
+router.get('/logout', (req, res) => {
+  res.clearCookie('jwt'); // Eliminar cookie
+  res.redirect('/'); // Redirige al login
+  console.log("Cerro sesion, el token fue eliminado");
+});
+
+module.exports = router;
